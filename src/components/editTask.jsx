fc63067b625e2aae4259f8e07b560ce1e36daccd
danilo90lan <15630@coderacademy.edu.au>
2024-11-30 10:47:57 +1100

    delete task feat

--- src/components/editTask.jsx
+++ src/components/editTask.jsx
@@ -5,7 +5,6 @@ export default function EditTask() {
     //use query param it returns the key value pair. It will be destructure with 
     // the curly brackets and get only the taskID
     const { taskId } = useParams();
-    console.log(typeof taskId);
 
     return (
         <div>
--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -6,6 +6,10 @@ import { useLocalStorage } from "react-use";
 export default function Tasks() {
     const [tasks, setTasks] = useLocalStorage("tasks", []);
 
+    const deleteTask = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id != id)
+        setTasks(updatedTasks);
+    }
 
     return (
         <div>
@@ -15,15 +19,16 @@ export default function Tasks() {
                     <p>No tasks found. <Link to="/add-task">Add your first task</Link></p>
                 ) : (
                     <ul>
-                        {tasks.map((task, index) => {
-                            return (
+                        {tasks.map((task, index) => 
+                            (
                                 <li key={index}>
                                     <strong>{task.title}</strong>:{task.description}
                                     <div>
-                                        <Link to={`/edit-task/${task.id}`}>Edit</Link> | {" "}
+                                        <Link to={`/edit-task/${task.id}`}>Edit</Link>
+                                        <button onClick={() => deleteTask(task.id)}>delete Task</button>
                                     </div>
                                 </li>)
-                        }
+                        
                         )}
                     </ul>
                 )
